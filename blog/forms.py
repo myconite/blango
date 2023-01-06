793fa6928aee830f9a71dcc8ce32288035f46ee1
myconite <41683442+myconite@users.noreply.github.com>
2023-01-06 08:12:18 +0100

    Update forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -8,8 +8,8 @@ class CommentForm(forms.ModelForm):
     model = Comment
     fields = ["content"]
 
-  def __init__(self, *args, **kwargs):
-    super(CommentForm, self).__init__(*args, **kwargs)
-    self.helper = FormHelper()
-    self.helper.add_input(Submit('submit', 'Submit'))
+  def __init__(self, *args, **kwargs): # Define the constructor
+    super(CommentForm, self).__init__(*args, **kwargs) # Call the constructor of the parent class
+    self.helper = FormHelper() # Instantiate self.helper as an instance of the FormHelper class
+    self.helper.add_input(Submit('submit', 'Submit')) # Instantiate an instance of the Submit class and add the submit button to the input of the form
 
