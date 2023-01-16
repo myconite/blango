c6b45bf7e102809e40f9a4f899ccabc39b30fa29
Urban Sotensek <urbansotensek@musician.org>
2023-01-16 11:22:56 +0000

    Finish permissions

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics
+from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
+
 
 from blog.api.serializers import PostSerializer
 from blog.models import Post
@@ -12,4 +14,5 @@ class PostList(generics.ListCreateAPIView):
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
 
